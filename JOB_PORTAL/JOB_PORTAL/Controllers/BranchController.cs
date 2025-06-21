43a8e19a57bf8d9da97609b80421b3d5d6c6fb2d
= <praneethabandaru01@gmail.com>
2025-06-21 18:00:55 +0530

    Praneetha : Created Branch Controller

---
+++ JOB_PORTAL/JOB_PORTAL/Controllers/BranchController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JOB_PORTAL.Controllers
+{
+    public class BranchController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddEmployer()
+        {
+            
+            return View();
+        }
+        [HttpGet]
+        public IActionResult DisplayEmployer()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult DisplayJob()
+        {
+            return View();
+        }
+
+
+    }
+}
--- JOB_PORTAL/JOB_PORTAL/Controllers/CompanyController.cs
+++ JOB_PORTAL/JOB_PORTAL/Controllers/CompanyController.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JOB_PORTAL.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JOB_PORTAL.Controllers
 {
     public class CompanyController : Controller
     {
+        JOB_PORTALContext obj = new JOB_PORTALContext();
         public IActionResult Index()
         {
             return View();
         }
+       
     }
 }
